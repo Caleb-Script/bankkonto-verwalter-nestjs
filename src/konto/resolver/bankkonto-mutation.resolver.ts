3912bb8621c8229543a7311f855f2162d41dd715
Alicia Schleise <alicia.schleise31@gmail.com>
2024-10-30 17:59:16 +0100

    Query-Resolver Fehler behoben und im Query-Builder Fehler durch ändern der Suchkriterien verbessert

--- src/konto/resolver/bankkonto-mutation.resolver.ts
+++ src/konto/resolver/bankkonto-mutation.resolver.ts
@@ -5,13 +5,13 @@ import { AuthGuard, Roles } from 'nest-keycloak-connect';
 import { getLogger } from '../../logger/logger.js';
 import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
 import { BankkontoDTO } from '../model/dto/bankkonto.dto.js';
-import { TransaktionDTO } from '../model/dto/transaktion.dto.js';
+import { type Transaktion } from '../model/entity/transaktion.entity.js';
 import { type Bankkonto } from '../model/entity/bankkonto.entity.js';
 import { type Kunde } from '../model/entity/kunde.entity.js';
-import { type Transaktion } from '../model/entity/transaktion.entity.js';
 import { BankkontoWriteService } from '../service/bankkonto-write.service.js';
 import { type IdInput } from './bankkonto-query.resolver.js';
 import { HttpExceptionFilter } from './http-exception.filter.js';
+import { TransaktionDTO } from '../model/dto/transaktion.dto.js';
 
 export type CreatePayload = {
     readonly kontoId: number;
@@ -37,7 +37,7 @@ export class BankkontoUpdateDTO extends BankkontoDTO {
 export class BankkontoMutationResolver {
     readonly #service: BankkontoWriteService;
 
-    readonly #logger = getLogger(BankkontoMutationResolver.name);
+    readonly #logger= getLogger(BankkontoMutationResolver.name);
 
     constructor(service: BankkontoWriteService) {
         this.#service = service;
@@ -76,14 +76,11 @@ export class BankkontoMutationResolver {
 
     @Mutation()
     @Roles({ roles: ['admin'] })
-    async delete(@Args() bankkontoId: IdInput) {
-        const idStr = bankkontoId.bankkontoId;
-        this.#logger.debug('delete: bankkontoId=%s', idStr);
+    async delete(@Args() kontoId: IdInput) {
+        const idStr = kontoId.kontoId;
+        this.#logger.debug('delete: kontoId=%s', idStr);
         const deletePerformed = await this.#service.delete(idStr);
-        this.#logger.debug(
-            'deleteBankkonto: deletePerformed=%s',
-            deletePerformed,
-        );
+        this.#logger.debug('deleteBankkonto: deletePerformed=%s', deletePerformed);
         return deletePerformed;
     }
 
@@ -97,20 +94,18 @@ export class BankkontoMutationResolver {
             bankkonto: undefined,
         };
         // "Optional Chaining" ab ES2020
-        const transaktionen = bankkontoDTO.transaktionen?.map(
-            (transaktionDTO: TransaktionDTO) => {
-                const transaktion: Transaktion = {
-                    transaktionId: undefined,
-                    transaktionDatum: undefined,
-                    betrag: transaktionDTO.betrag,
-                    empfaenger: transaktionDTO.empfaenger,
-                    absender: transaktionDTO.absender,
-                    transaktionTyp: undefined,
-                    bankkonto: undefined,
-                };
-                return transaktion;
-            },
-        );
+        const transaktionen = bankkontoDTO.transaktionen?.map((transaktionDTO: TransaktionDTO) => {
+            const transaktion: Transaktion = {
+                transaktionId: undefined,
+                transaktionDatum: undefined,
+                betrag: transaktionDTO.betrag,
+                empfaenger: transaktionDTO.empfaenger,
+                absender: transaktionDTO.absender,
+                transaktionTyp: undefined,
+                bankkonto: undefined,
+            };
+            return transaktion;
+        });
         const bankkonto: Bankkonto = {
             bankkontoId: undefined,
             version: undefined,
