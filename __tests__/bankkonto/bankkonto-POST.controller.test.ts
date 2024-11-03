1113bea02b8c4d5d2892dda32d1864514a793287
Caleb-Script <caleb-script@outlook.de>
2024-11-03 21:33:03 +0100

    POST TEST
    - bankkonto.dto angepasst
    - writeController angepasst
    - mutation.resolver angepasst
    - gitignore

--- __tests__/bankkonto/bankkonto-POST.controller.test.ts
+++ __tests__/bankkonto/bankkonto-POST.controller.test.ts
@@ -1,9 +1,8 @@
 import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 import { HttpStatus } from '@nestjs/common';
 import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
-import { BankkontoDTO } from '../../src/bankkonto/model/dto/bankkonto.dto.js';
-import { type BuchDTO } from '../../src/buch/controller/buchDTO.entity.js';
-import { BuchReadService } from '../../src/buch/service/buch-read.service.js';
+import { type BankkontoDTO } from '../../src/bankkonto/model/dto/bankkonto.dto.js';
+import { BankkontoReadService } from '../../src/bankkonto/service/bankkonto-read.service.js';
 import {
     host,
     httpsAgent,
@@ -12,70 +11,36 @@ import {
     startServer,
 } from '../testserver.js';
 import { tokenRest } from '../token.js';
-import { type ErrorResponse } from './error-response.js';
 
 // -----------------------------------------------------------------------------
 // T e s t d a t e n
 // -----------------------------------------------------------------------------
 const neuesBankkonto: BankkontoDTO = {
-    transaktionsLimit: 150,
-    waehrungen: ['EUR'],
+    transaktionsLimit: 100,
+    waehrungen: ['EUR', 'USD'],
     kunde: {
-        vorname: 'Max',
-        name: 'Mustermann',
-        email: 'max@example.com',
+        name: 'Jefferson',
+        vorname: 'Rolly',
+        email: 'JR@ok.de',
     },
 };
-const neuesBuch: BuchDTO = {
-    isbn: '978-0-007-00644-1',
-    rating: 1,
-    art: 'EPUB',
-    preis: 99.99,
-    rabatt: 0.123,
-    lieferbar: true,
-    datum: '2022-02-28',
-    homepage: 'https://post.rest',
-    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
-    titel: {
-        titel: 'Titelpost',
-        untertitel: 'untertitelpos',
+// const neuesBankkontoKundeExistiert: BankkontoDTO = {
+//     transaktionsLimit: 10,
+//     waehrungen: ['EUR'],
+//     kunde: {
+//         name: 'Jefferson',
+//         vorname: 'Rolly',
+//         email: 'JR@ok.de',
+//     },
+// };
+const neuesBankkontoInvalid: Record<string, unknown> = {
+    transaktionsLimit: -1,
+    waehrungen: ['FAKE'],
+    kunde: {
+        name: '',
+        vorname: '',
+        email: 'invalidEmail',
     },
-    abbildungen: [
-        {
-            beschriftung: 'Abb. 1',
-            contentType: 'img/png',
-        },
-    ],
-};
-const neuesBuchInvalid: Record<string, unknown> = {
-    isbn: 'falsche-ISBN',
-    rating: -1,
-    art: 'UNSICHTBAR',
-    preis: -1,
-    rabatt: 2,
-    lieferbar: true,
-    datum: '12345-123-123',
-    homepage: 'anyHomepage',
-    titel: {
-        titel: '?!',
-        untertitel: 'Untertitelinvalid',
-    },
-};
-const neuesBuchIsbnExistiert: BuchDTO = {
-    isbn: '978-3-897-22583-1',
-    rating: 1,
-    art: 'EPUB',
-    preis: 99.99,
-    rabatt: 0.099,
-    lieferbar: true,
-    datum: '2022-02-28',
-    homepage: 'https://post.isbn/',
-    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
-    titel: {
-        titel: 'Titelpostisbn',
-        untertitel: 'Untertitelpostisbn',
-    },
-    abbildungen: undefined,
 };
 
 // -----------------------------------------------------------------------------
@@ -112,7 +77,7 @@ describe('POST /rest', () => {
         // when
         const response: AxiosResponse<string> = await client.post(
             '/rest',
-            neuesBuch,
+            neuesBankkonto,
             { headers },
         );
 
@@ -133,30 +98,27 @@ describe('POST /rest', () => {
         const idStr = location.slice(indexLastSlash + 1);
 
         expect(idStr).toBeDefined();
-        expect(BuchReadService.ID_PATTERN.test(idStr)).toBe(true);
+        expect(BankkontoReadService.ID_PATTERN.test(idStr)).toBe(true);
 
         expect(data).toBe('');
     });
 
-    test('Neues Buch mit ungueltigen Daten', async () => {
+    test('Neues Bankkonto mit ungueltigen Daten', async () => {
         // given
         const token = await tokenRest(client);
         headers.Authorization = `Bearer ${token}`;
         const expectedMsg = [
-            expect.stringMatching(/^isbn /u),
-            expect.stringMatching(/^rating /u),
-            expect.stringMatching(/^art /u),
-            expect.stringMatching(/^preis /u),
-            expect.stringMatching(/^rabatt /u),
-            expect.stringMatching(/^datum /u),
-            expect.stringMatching(/^homepage /u),
-            expect.stringMatching(/^titel.titel /u),
+            // expect.stringMatching(/^transaktionsLimit /u),
+            // expect.stringMatching(/^waehrungen /u),
+            expect.stringMatching(/^kunde.name /u),
+            expect.stringMatching(/^kunde.vorname /u),
+            expect.stringMatching(/^kunde.email /u),
         ];
 
         // when
         const response: AxiosResponse<Record<string, any>> = await client.post(
             '/rest',
-            neuesBuchInvalid,
+            neuesBankkontoInvalid,
             { headers },
         );
 
@@ -173,39 +135,40 @@ describe('POST /rest', () => {
         expect(messages).toEqual(expect.arrayContaining(expectedMsg));
     });
 
-    test('Neues Buch, aber die ISBN existiert bereits', async () => {
-        // given
-        const token = await tokenRest(client);
-        headers.Authorization = `Bearer ${token}`;
+    // TODO duplicate ERROR implementieren
+    // test('Neues Bankkonto, aber der Kunde existiert bereits', async () => {
+    //     // given
+    //     const token = await tokenRest(client);
+    //     headers.Authorization = `Bearer ${token}`;
 
-        // when
-        const response: AxiosResponse<ErrorResponse> = await client.post(
-            '/rest',
-            neuesBuchIsbnExistiert,
-            { headers },
-        );
+    //     // when
+    //     const response: AxiosResponse<ErrorResponse> = await client.post(
+    //         '/rest',
+    //         neuesBankkontoKundeExistiert,
+    //         { headers },
+    //     );
 
-        // then
-        const { data } = response;
+    //     // then
+    //     const { data } = response;
 
-        const { message, statusCode } = data;
+    //     const { message, statusCode } = data;
 
-        expect(message).toEqual(expect.stringContaining('ISBN'));
-        expect(statusCode).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
-    });
+    //     expect(message).toEqual(expect.stringContaining('kunde'));
+    //     expect(statusCode).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+    // });
 
-    test('Neues Buch, aber ohne Token', async () => {
+    test('Neues Bankkonto, aber ohne Token', async () => {
         // when
         const response: AxiosResponse<Record<string, any>> = await client.post(
             '/rest',
-            neuesBuch,
+            neuesBankkonto,
         );
 
         // then
         expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
     });
 
-    test('Neues Buch, aber mit falschem Token', async () => {
+    test('Neues Bankkonto, aber mit falschem Token', async () => {
         // given
         const token = 'FALSCH';
         headers.Authorization = `Bearer ${token}`;
@@ -213,7 +176,7 @@ describe('POST /rest', () => {
         // when
         const response: AxiosResponse<Record<string, any>> = await client.post(
             '/rest',
-            neuesBuch,
+            neuesBankkonto,
             { headers },
         );
 
